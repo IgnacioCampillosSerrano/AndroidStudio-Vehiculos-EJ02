65b74c928ff0b4d7f0818c53750ef07995b9e20e
ignaciocampillosserrano <ignacio.campillos.serrano@cieep.com>
2023-09-27 11:20:49 +0200

    Initial commit

---
+++ app/src/main/java/ignacio/campillos/androidstudio_vehiculos_ej02/MotoActivity.java
@@ -0,0 +1,14 @@
+package ignacio.campillos.androidstudio_vehiculos_ej02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moto);
+    }
+}
